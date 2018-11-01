cb1015876450d63105a8821deebb61f1620df523
Justin Moss <Justin.Moss@gmail.com>
2018-11-01 17:52:17 -0400

    Add DataContract DotNetCore example

---
+++ Example.Serialization.DataContract.DotNetCore/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.Serialization;
+using RockLib.Serialization;
+
+namespace Example.Serialization.DataContract.DotNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var item = new ParentExampleClass
+            {
+                DateProperty = DateTime.Now,
+                ClassProperty = new NestedExampleClass
+                {
+                    IntProperty = 15,
+                    BoolProperty = true,
+                    StringProperty = "The string to serialize"
+                }
+            };
+
+            var json = item.ToJson();
+
+            Console.WriteLine("Serialized Json:");
+            Console.WriteLine(json);
+
+            var jsonDeserialized = json.FromJson<ParentExampleClass>();
+
+            Console.WriteLine();
+            if (item.DateProperty.ToString("O") == jsonDeserialized.DateProperty.ToString("O")
+                && item.ClassProperty.IntProperty == jsonDeserialized.ClassProperty.IntProperty
+                && item.ClassProperty.BoolProperty == jsonDeserialized.ClassProperty.BoolProperty
+                && item.ClassProperty.StringProperty == jsonDeserialized.ClassProperty.StringProperty)
+                Console.WriteLine("Json Deserialization Result: Success");
+            else
+                Console.WriteLine("Json Deserialization Result: Failed");
+
+            var xml = item.ToXml();
+
+            Console.WriteLine();
+            Console.WriteLine("Serialized Xml:");
+            Console.WriteLine(xml);
+
+            var xmlDeserialized = xml.FromXml<ParentExampleClass>();
+
+            Console.WriteLine();
+            if (item.DateProperty.ToString("O") == xmlDeserialized.DateProperty.ToString("O")
+                && item.ClassProperty.IntProperty == xmlDeserialized.ClassProperty.IntProperty
+                && item.ClassProperty.BoolProperty == xmlDeserialized.ClassProperty.BoolProperty
+                && item.ClassProperty.StringProperty == xmlDeserialized.ClassProperty.StringProperty)
+                Console.WriteLine("Xml Deserialization Result: Success");
+            else
+                Console.WriteLine("Xml Deserialization Result: Failed");
+
+            Console.ReadKey();
+        }
+    }
+
+    [DataContract]
+    public class ParentExampleClass
+    {
+        [DataMember]
+        public DateTime DateProperty { get; set; }
+        [DataMember]
+        public NestedExampleClass ClassProperty { get; set; }
+    }
+
+    [DataContract]
+    public class NestedExampleClass
+    {
+        [DataMember]
+        public int IntProperty { get; set; }
+        [DataMember]
+        public bool BoolProperty { get; set; }
+        [DataMember]
+        public string StringProperty { get; set; }
+    }
+}
