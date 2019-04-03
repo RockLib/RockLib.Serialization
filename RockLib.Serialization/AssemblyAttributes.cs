e729bf2b7e3262f3e07ba349ab0c946eec4697ff
Justin Moss <Justin.Moss@gmail.com>
2019-04-02 20:46:15 -0400

    Add ConfigSection attribute for Rockifier tool

---
+++ RockLib.Serialization/AssemblyAttributes.cs
@@ -0,0 +1,6 @@
+﻿using RockLib.Configuration.ObjectFactory;
+using RockLib.Serialization;
+using System.Collections.Generic;
+
+[assembly: ConfigSection("RockLib.Serialization:JsonSerializers", typeof(List<ISerializer>))]
+[assembly: ConfigSection("RockLib.Serialization:XmlSerializers", typeof(List<ISerializer>))]
