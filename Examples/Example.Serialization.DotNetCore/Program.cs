da593703fe8a8b4638d51ea36729d3c7a228de54
Jason Bock <JasonBock1@rocketmortgage.com>
2022-11-14 09:47:00 -0500

    #127 updated the Example.Serialization example as well

--- Examples/Example.Serialization.DotNetCore/Program.cs
+++ Examples/Example.Serialization.DotNetCore/Program.cs
@@ -1,70 +1,73 @@
 ﻿using System;
 using RockLib.Serialization;
 
-namespace Example.Serialization.DotNetCore
+var item = new ParentExampleClass
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var item = new ParentExampleClass
-            {
-                DateProperty = DateTime.Now,
-                ClassProperty = new NestedExampleClass
-                {
-                    IntProperty = 15,
-                    BoolProperty = true,
-                    StringProperty = "The string to serialize"
-                }
-            };
+	DateProperty = DateTime.Now,
+	ClassProperty = new NestedExampleClass
+	{
+		IntProperty = 15,
+		BoolProperty = true,
+		StringProperty = "The string to serialize"
+	}
+};
 
-            var json = item.ToJson();
+var json = item.ToJson();
 
-            Console.WriteLine("Serialized Json:");
-            Console.WriteLine(json);
+Console.WriteLine("Serialized Json:");
+Console.WriteLine(json);
 
-            var jsonDeserialized = json.FromJson<ParentExampleClass>();
+var jsonDeserialized = json.FromJson<ParentExampleClass>();
 
-            Console.WriteLine();
-            if (item.DateProperty.ToString("O") == jsonDeserialized.DateProperty.ToString("O")
-                && item.ClassProperty.IntProperty == jsonDeserialized.ClassProperty.IntProperty
-                && item.ClassProperty.BoolProperty == jsonDeserialized.ClassProperty.BoolProperty
-                && item.ClassProperty.StringProperty == jsonDeserialized.ClassProperty.StringProperty)
-                Console.WriteLine("Json Deserialization Result: Success");
-            else
-                Console.WriteLine("Json Deserialization Result: Failed");
+Console.WriteLine();
+if (item.DateProperty.ToString("O") == jsonDeserialized.DateProperty.ToString("O")
+	 && item.ClassProperty.IntProperty == jsonDeserialized.ClassProperty!.IntProperty
+	 && item.ClassProperty.BoolProperty == jsonDeserialized.ClassProperty.BoolProperty
+	 && item.ClassProperty.StringProperty == jsonDeserialized.ClassProperty.StringProperty)
+{
+	Console.WriteLine("Json Deserialization Result: Success");
+}
+else
+{
+	Console.WriteLine("Json Deserialization Result: Failed");
+}
 
-            var xml = item.ToXml();
+var xml = item.ToXml();
 
-            Console.WriteLine();
-            Console.WriteLine("Serialized Xml:");
-            Console.WriteLine(xml);
+Console.WriteLine();
+Console.WriteLine("Serialized Xml:");
+Console.WriteLine(xml);
 
-            var xmlDeserialized = xml.FromXml<ParentExampleClass>();
+var xmlDeserialized = xml.FromXml<ParentExampleClass>();
 
-            Console.WriteLine();
-            if (item.DateProperty.ToString("O") == xmlDeserialized.DateProperty.ToString("O")
-                && item.ClassProperty.IntProperty == xmlDeserialized.ClassProperty.IntProperty
-                && item.ClassProperty.BoolProperty == xmlDeserialized.ClassProperty.BoolProperty
-                && item.ClassProperty.StringProperty == xmlDeserialized.ClassProperty.StringProperty)
-                Console.WriteLine("Xml Deserialization Result: Success");
-            else
-                Console.WriteLine("Xml Deserialization Result: Failed");
+Console.WriteLine();
+if (item.DateProperty.ToString("O") == xmlDeserialized.DateProperty.ToString("O")
+	 && item.ClassProperty.IntProperty == xmlDeserialized.ClassProperty!.IntProperty
+	 && item.ClassProperty.BoolProperty == xmlDeserialized.ClassProperty.BoolProperty
+	 && item.ClassProperty.StringProperty == xmlDeserialized.ClassProperty.StringProperty)
+{
+	Console.WriteLine("Xml Deserialization Result: Success");
+}
+else
+{
+	Console.WriteLine("Xml Deserialization Result: Failed");
+}
 
-            Console.ReadKey();
-        }
-    }
+Console.ReadKey();
 
-    public class ParentExampleClass
-    {
-        public DateTime DateProperty { get; set; }
-        public NestedExampleClass ClassProperty { get; set; }
-    }
+#pragma warning disable CA1050 // Declare types in namespaces
+public class ParentExampleClass
+#pragma warning restore CA1050 // Declare types in namespaces
+{
+	public DateTime DateProperty { get; set; }
+	public NestedExampleClass? ClassProperty { get; set; }
+}
 
-    public class NestedExampleClass
-    {
-        public int IntProperty { get; set; }
-        public bool BoolProperty { get; set; }
-        public string StringProperty { get; set; }
-    }
+#pragma warning disable CA1050 // Declare types in namespaces
+public class NestedExampleClass
+#pragma warning restore CA1050 // Declare types in namespaces
+{
+	public int IntProperty { get; set; }
+	public bool BoolProperty { get; set; }
+	public string? StringProperty { get; set; }
 }
